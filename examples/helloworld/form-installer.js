b5125026d88f6091cc820fafd935b99f4b888f7f
Marak Squires <marak.squires@gmail.com>
2012-06-11 23:51:48 -0700

    [refactor] Rename http to web. Cleaned up code.

---
+++ examples/helloworld/form-installer.js
@@ -0,0 +1,33 @@
+var wizard = require('../../lib/wizard'),
+    fs = require('fs');
+
+//
+// Load schema object from config.schema file ( regular JSON-schema)
+//
+var schema = wizard.load('./config/schema.json');
+
+//
+// Run the CLI configuration wizard 
+//
+wizard.web.run(schema, function(err, results){
+
+  if(err){
+    console.log(err);
+  }
+
+  //
+  // After the wizard completes, write the results to the config.json file
+  //
+
+  fs.writeFileSync('./config.json', JSON.stringify(results));
+
+  //
+  // Now that the app is configured, see if user wants to start it!
+  // 
+  
+  //
+  // TODO: Cli prompt for starting
+  //
+
+});
+
